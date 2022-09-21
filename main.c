88fb51c3fcc2bc8c831222b5263d241b2b5adb46
Cortex0101 <ldeiruff@gmail.com>
2022-09-21 14:57:41 +0200

    Copy pasted code

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+void prompt() {
+
+}
+
+void read_command(const char* command, const char** parameters) {
+
+}
 
 int main() {
-    printf("Hello, World!\n");
+    const char* command = "test()";
+    const char* parameters = "blah";
+
+    while (1) {
+        prompt();
+        read_command(command, &parameters);
+
+        if (fork() != 0) {
+            // parent code
+            waitpid(-1, &status, 0);
+        } else {
+            execve(command, parameters, 0);
+        }
+    }
+
     return 0;
 }
